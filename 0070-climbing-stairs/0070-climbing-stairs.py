6722afc02c6fcb94ca9d7708f43c91c7c0043c99
Błażej Radzik <262807634+BlazejRadzik@users.noreply.github.com>
2026-06-03 04:05:03 +0200

    Time: 0 ms (100%), Space: 12.2 MB (90.25%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def climbStairs(self, n):
+
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        two_steps_behind = 1
+        one_step_behind = 2   
+        
+        for i in range(3, n + 1):
+            current = one_step_behind + two_steps_behind
+            
+            two_steps_behind = one_step_behind
+            one_step_behind = current
+            
+        return one_step_behind
