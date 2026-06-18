09e8ae7bb123394efd33dead3297780125fa0644
Błażej Radzik <blazej.radzik@edu.uekat.pl>
2026-06-18 22:48:34 +0200

    Time: 0 ms (100%), Space: 19 MB (77.37%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+        
+        return 1 + std::max(leftDepth, rightDepth);
+    }
+};
