4240ff5ce6b9f46757f9fd63c2de9415ee21a77c
Błażej Radzik <blazej.radzik@edu.uekat.pl>
2026-06-19 19:09:52 +0200

    Time: 0 ms (100%), Space: 23.1 MB (54.71%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return checkHeight(root) != -1;
+    }
+
+private:
+    int checkHeight(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(node->left);
+        if (leftHeight == -1) {
+            return -1; 
+        }
+
+        int rightHeight = checkHeight(node->right);
+        if (rightHeight == -1) {
+            return -1; 
+        }
+
+        if (std::abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return std::max(leftHeight, rightHeight) + 1;
+    }
+};
