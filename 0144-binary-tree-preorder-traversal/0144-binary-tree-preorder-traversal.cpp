62c91ac9b98da0e78db8ef28a77fb2898dbf6262
Błażej Radzik <blazej.radzik@edu.uekat.pl>
2026-06-24 19:54:28 +0200

    Time: 0 ms (100%), Space: 10.9 MB (42.51%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void traverse(TreeNode* node, vector<int>& result) {
+        if (!node) return;
+        
+        result.push_back(node->val);
+        traverse(node->left, result);
+        traverse(node->right, result);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traverse(root, result);
+        return result;
+    }
+};
